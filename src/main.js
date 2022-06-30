ffd21a14507bf63f991459e8eb812cec8790a2ca
黄伟 <790679955@qq.com>
2022-06-30 14:15:47 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 import router from './router'
 import store from './store'
+import ViewUI from 'view-design';
+import 'view-design/dist/styles/iview.css';
 
 Vue.config.productionTip = false
+Vue.use(ElementUI);
+Vue.use(ViewUI);
 
 new Vue({
   router,
